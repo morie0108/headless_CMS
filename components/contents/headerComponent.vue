c0e104537184d71f57b3dc38697d1c44d3451cf8
Yohei <morizane.yo@gmail.com>
2021-12-11 19:26:00 +0900

    add footer

--- components/contents/headerComponent.vue
+++ components/contents/headerComponent.vue
@@ -1,22 +1,42 @@
 <template>
-  <v-app-bar class="header-bar main">
-    <v-toolbar-title class="header-title">
-      <nuxt-link to="/">Vuetify</nuxt-link>
-    </v-toolbar-title>
-  </v-app-bar>
+  <header>
+    <v-app-bar
+      app
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>MyPortfolioSite</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+    v-model="drawer"
+    fixed
+    temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group>
+          <v-list-item
+            v-for="(menuItem, index) in menuItems"
+            :key="index"
+          >
+            <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
 </template>
 
 <script>
+import constants from '~/common/constants'
+
 export default {
+  data () {
+    return {
+      drawer: false,
+      menuItems: constants.menuItems
+    }
+  }
 }
 </script>
-
-<style lang="scss" scoped>
-
-.header-bar{
-  background: #d7edff;
-  position: fixed;
-  top: 0;
-  z-index: 5;
-}
-</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,50 +1,20 @@
 <template>
   <v-app>
-    <!-- <headerComponent /> -->
-      <header>
-        <v-app-bar
-          app
-        >
-          <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-          <v-toolbar-title>MyPortfolioSite</v-toolbar-title>
-        </v-app-bar>
-        <v-navigation-drawer
-        v-model="drawer"
-        fixed
-        temporary
-        >
-        <v-list
-          nav
-          dense
-        >
-          <v-list-item-group>
-            <v-list-item
-              v-for="(menuItem, index) in menuItems"
-              :key="index"
-            >
-              <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-navigation-drawer>
-      </header>
+    <headerComponent />
     <nuxt />
+    <footerComponent />
   </v-app>
 </template>
 
 <script>
-import constants from '~/common/constants'
+import headerComponent from '~/components/contents/headerComponent'
+import footerComponent from '~/components/contents/footerComponent'
 
 export default {
   name: 'default',
-  data () {
-    return {
-      drawer: false,
-      menuItems: constants.menuItems
-    }
+  components: {
+    headerComponent,
+    footerComponent
   }
-  // components: {
-  //   headerComponent
-  // }
 }
 </script>
